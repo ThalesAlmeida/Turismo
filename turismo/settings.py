2b0c4c94f5f308dcc7c353329bfa7007d419168b
Thales <thalesalves44@gmail.com>
2019-04-24 20:27:57 -0300

    adicionando localhost ao allowed hosts

--- turismo/settings.py
+++ turismo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['apipontosturisticos.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['apipontosturisticos.herokuapp.com', 'localhost:8000', 'localhost']
 
 
 # Application definition
